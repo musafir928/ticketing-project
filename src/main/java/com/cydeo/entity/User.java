46506da8b8cf82b430ba25bf2c5c69139f1caab7
musafir928 <uganoghli@gmail.com>
2022-03-25 00:41:17 +0100

    Initial commit

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Data
+public class User extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+    // note: even we use lombok for constructor, child class is not automatically
+    // going to define all arg constructor including super class constructor
+    // so we do it manually
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String password, boolean enabled, String phone, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
+}
