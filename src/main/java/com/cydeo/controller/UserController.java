fd2e61c7de69a649b8a4b0cc09c1c22146654512
musafir928 <uganoghli@gmail.com>
2022-03-25 12:24:45 +0100

    form data binding(th:object - field)

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     @GetMapping("/create")
     public String createUser(Model model) {
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", listOfRoles);
         return "user/create";
     }
 
