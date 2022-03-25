6a935ff5c2335936bdb097aa27d586dfc3cbe626
musafir928 <uganoghli@gmail.com>
2022-03-25 12:42:15 +0100

    add crud service interface

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,15 +5,4 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService {
-    /**
-     * functionalities:
-     * save, findById, delete, findAll
-     * */
-
-    RoleDTO save(RoleDTO role);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void delete(RoleDTO role);
-    void deleteById(Long id);
-}
+public interface RoleService extends CrudService<RoleDTO, Long> {}
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,11 +4,4 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService {
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void delete(UserDTO user);
-    void deleteById(String username);
-
-}
+public interface UserService extends CrudService<UserDTO, String>{}
