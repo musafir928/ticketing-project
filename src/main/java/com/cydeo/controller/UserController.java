94ae9f3d542ab0b3b59c79f6a61a171a4f2bed59
musafir928 <uganoghli@gmail.com>
2022-03-30 02:59:49 +0200

    data converter

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,12 +24,21 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String createUser(Model model) {
+    public String getCreateUserForm(Model model) {
+
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
 
         return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String createUser( UserDTO user, Model model) {
+
+        userService.save(user);
+
+        return "redirect:create";
     }
 
 }
