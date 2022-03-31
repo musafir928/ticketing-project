f4a6fd9335b4ecb92a4b4a002cc75345e85a3ca4
musafir928 <uganoghli@gmail.com>
2022-04-01 01:27:51 +0200

    update project service

--- src/main/java/com/cydeo/service/impl/ProjectServiceIMPL.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceIMPL.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ public class ProjectServiceIMPL extends AbstractMapServices<String, ProjectDTO> 
 
     @Override
     public ProjectDTO save(ProjectDTO object) {
+        if (object.getStatus() == null) {
+            object.setStatus(Status.OPEN);
+        }
         return super.save(object.getProjectCode(), object);
     }
 
