c3c0d78e4ece31cb2c25d8b16f3e6915ce5c8a9a
musafir928 <uganoghli@gmail.com>
2022-04-02 18:33:05 +0200

    update DTO

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -21,4 +21,17 @@ public class ProjectDTO {
     private LocalDate endDate;
     private String projectDetail;
     private Status status;
+
+    private int completeTaskCounts;
+    private int unfinishedTaskCounts;
+
+    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status status) {
+        this.projectName = projectName;
+        this.projectCode = projectCode;
+        this.assignedManager = assignedManager;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.projectDetail = projectDetail;
+        this.status = status;
+    }
 }
