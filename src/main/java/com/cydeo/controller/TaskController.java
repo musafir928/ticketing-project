51ae1bb1bd1839b0dac243cd5c3595582286e2aa
musafir928 <uganoghli@gmail.com>
2022-04-02 18:31:59 +0200

    build task controller

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,15 +1,107 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
+    ProjectService projectService;
+    UserService userService;
+    TaskService taskService;
+
+    public TaskController(ProjectService projectService, UserService userService, TaskService taskService) {
+        this.projectService = projectService;
+        this.userService = userService;
+        this.taskService = taskService;
+    }
+
     @GetMapping("/create")
     public String getCreateTaskForm (Model model) {
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("tasks", taskService.findAll());
         return "task/create";
+    }
+
+    @PostMapping("/create")
+    public String createTask(TaskDTO task) {
+        taskService.save(task);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/delete/{taskId}")
+    public String deleteTask(@PathVariable Long taskId) {
+        taskService.deleteById(taskId);
+        return "redirect:/task/create";
+    }
+
+
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
+
+        model.addAttribute("task", taskService.findById(taskId));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+
+        return "task/update";
+
+    }
+
+//    @PostMapping("/update/{taskId}")
+//    public String updateTask(@PathVariable Long taskId, TaskDTO task) {
+//        task.setId(taskId);
+//        taskService.update(task);
+//        return "redirect:/task/create";
+//    }
+
+    // simplified version of above method
+    // spring get the field value from path param automatically when the name is the same (id=id, bur not taskId!=id)
+    @PostMapping("/update/{id}")
+    public String updateTask(TaskDTO task) {
+        taskService.update(task);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/employee/pending-tasks")
+    public String employeePendingTasks(Model model) {
+        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+        return "task/pending-tasks";
+    }
+
+    @GetMapping("/employee/edit/{id}")
+    public String employeeEditTask(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("task", taskService.findById(id));
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("projects", projectService.findAllNonCompletedProjects());
+        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
+        model.addAttribute("statuses", Status.values());
+
+        return "task/status-update";
+
+    }
+
+    @PostMapping("/employee/update/{id}")
+    public String employeeUpdateTask(TaskDTO taskDTO) {
+        taskService.updateStatus(taskDTO);
+        return "redirect:/task/employee/pending-tasks";
+    }
+
+    @GetMapping("/employee/archive")
+    public String employeeArchivedTasks(Model model) {
+        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
+        return "task/archive";
     }
 }
